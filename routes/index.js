61f22a3e67564513b9f5ab09756122698ba3c86f
Ahmad Sarian <sakirsyarian@gmail.com>
2023-08-13 20:08:57 +0700

    feat: Create get, post, and delete data for project

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const Controller = require('../controllers');
 const testimonialRouter = require('./testimonialRoute');
 const abilityRouter = require('./abilityRoute');
+const projectRouter = require('./projectRoute');
 
 router.get('/', Controller.home);
 router.use(testimonialRouter);
 router.use(abilityRouter);
+router.use(projectRouter);
 
 module.exports = router;
