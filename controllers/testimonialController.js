62d99312a90d1eae8c964a44a0404054ece3122e
Ahmad Sarian <sakirsyarian@gmail.com>
2023-08-15 12:29:25 +0700

    feat: Testing for testimonial

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -12,14 +12,14 @@ class TestimonialController {
 
     static async save(req, res) {
         try {
-            const { image, name, role, message } = req.body;
+            const { image, name, role, message, test } = req.body;
 
-            const createTestimonial = new Testimonial({ image, name, role, message });
+            const createTestimonial = new Testimonial({ image, name, role, message, test });
             const testimonial = await createTestimonial.save();
 
             res.status(201).json({ success: true, data: testimonial });
         } catch (error) {
-            res.status(500).json({ success: false, error });
+            res.status(400).json(error);
         }
     }
 
@@ -28,7 +28,7 @@ class TestimonialController {
             const { id } = req.params;
             const testimonial = await Testimonial.findByIdAndDelete(id);
 
-            res.status(200).json({ success: true, data: testimonial });
+            res.status(200).json({ message: `${testimonial.name} deleted successfully` });
         } catch (error) {
             res.status(500).json({ success: false, error });
         }
