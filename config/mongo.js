4a9257ab327229c9770cc5372d8ec15d07435c8e
Ahmad Sarian <sakirsyarian@gmail.com>
2023-08-19 19:51:31 +0700

    refactor: Write proper connection with message

--- config/mongo.js
+++ config/mongo.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/portfolio', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+try {
+    const URI = 'mongodb://127.0.0.1:27017/portfolio';
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    const db = mongoose.connection;
+    db.once('open', () => {
+        console.log(`MongoDB database connection: ${URI}`);
+    });
+
+    db.on('error', (error) => {
+        console.log(`MongoDB connection error: ${error}`);
+    });
+} catch (error) {
+    console.log(error);
+}
 
 module.exports = mongoose;
