b61cab89106530deedd88a9a4d4901f6676203fe
Ahmad Sarian <sakirsyarian@gmail.com>
2023-09-04 13:25:47 +0700

    refactor: Change url mongo

--- config/mongo.js
+++ config/mongo.js
@@ -1,9 +1,14 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const mongoose = require('mongoose');
 
 const MONGO = process.env.MONGO || 27017;
+const URL_MONGO = process.env.URL_MONGO || `mongodb://127.0.0.1:${MONGO}/portfolio`;
 
 try {
-    const URI = `mongodb://127.0.0.1:${MONGO}/portfolio`;
+    const URI = URL_MONGO;
     mongoose.connect(URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -11,7 +16,7 @@ try {
 
     const db = mongoose.connection;
     db.once('open', () => {
-        console.log(`MongoDB database connection: ${URI}`);
+        console.log(`MongoDB database connection`);
     });
 
     db.on('error', (error) => {
